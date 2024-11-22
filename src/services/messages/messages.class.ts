d13135019a7818102d16c365109cff722ccabbaf
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-23 00:52:46 +0100

    handle visibility

--- src/services/messages/messages.class.ts
+++ src/services/messages/messages.class.ts
@@ -8,6 +8,7 @@ import type { Application } from '../../declarations'
 import type { Messages, MessagesData, MessagesPatch, MessagesQuery } from './messages.schema'
 import { app } from '../../app'
 import { conversations } from '../conversations/conversations'
+import { ObjectId } from 'mongodb'
 
 export type { Messages, MessagesData, MessagesPatch, MessagesQuery }
 
@@ -21,7 +22,29 @@ export class MessagesService<ServiceParams extends Params = MessagesParams> exte
   MessagesPatch
 > {
   async find(params: any): Promise<any> {
+    const currentUserId = params.query.currentUserId
+    if (!currentUserId) {
+      return []
+    }
+    //filter deleted not visible messages(deleted,sent when user had left for a moment)
+    const visibileMessages: any = await app.service('message-visibility').find({
+      query: {
+        userId: currentUserId,
+        conversationId: params.query.conversation
+      } as any,
+      paginate: false
+    })
+    const visibileMessagesIds = visibileMessages.map((v: any) => {
+      return new ObjectId(v.messageId)
+    })
+    //currentUserId not needed anymore
+    delete params.query.currentUserId
+    params.query = {
+      _id: { $in: visibileMessagesIds },
+      ...params.query
+    }
     const messages = await super.find(params)
+    //populate sender object
     for (const message of messages.data) {
       if (message.sender._id) {
         message.sender = await app.service('my-users').get(message.sender._id)
@@ -33,15 +56,18 @@ export class MessagesService<ServiceParams extends Params = MessagesParams> exte
     return messages
   }
   async create(body: any, params: any): Promise<any> {
+    let userMessage: any
     const conversation = await app.service('conversations').get(body.conversation)
+
+    //handl ai conversations
     if (conversation.type == 'ai') {
       const response = await ollama.chat({
         model: 'llama3:latest',
         messages: [{ role: 'user', content: body.text }]
       })
       //create user message
-      const create = await super.create(body, params)
-      create.sender = await app.service('my-users').get(body.sender)
+      userMessage = await super.create(body, params)
+      userMessage.sender = await app.service('my-users').get(body.sender)
       //create ai response message
 
       let aiUser = await app.service('my-users').find({
@@ -60,20 +86,48 @@ export class MessagesService<ServiceParams extends Params = MessagesParams> exte
         },
         params
       )
+      //set visibility
+      await this.setVisibility(userMessage, conversation)
 
       return {
-        myMessage: create,
+        myMessage: userMessage,
         aiMessage
       }
     } else {
-      return await super.create(body, params)
+      userMessage = await super.create(body, params)
+      //set visibility
+      await this.setVisibility(userMessage, conversation)
+      return userMessage
     }
+  }
+  async setVisibility(message: any, conversation: any) {
+    //set visibility
+    for (const member of conversation.members) {
+      const visibility = {
+        userId: member._id.toString(),
+        messageId: message._id.toString(),
+        conversationId: message.conversation
+      }
+
+      await app.service('message-visibility').create(visibility)
+    }
+  }
+  async remove(id: any, params: any): Promise<any> {
+    const convId = params.query.conversation
+    await app.service('message-visibility').remove(null, {
+      query: {
+        conversationId: convId
+      }
+    })
+
+    return await super.remove(id, params)
   }
 }
 
 export const getOptions = (app: Application): MongoDBAdapterOptions => {
   return {
     paginate: app.get('paginate'),
-    Model: app.get('mongodbClient').then((db) => db.collection('messages'))
+    Model: app.get('mongodbClient').then((db) => db.collection('messages')),
+    multi: ['remove'] // Enable bulk removal
   }
 }
