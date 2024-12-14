a8ec101c33edaa2afea0cda6f0b0d26fcc10fc10
YALNEZAMI <yaseralnezami@gmail.com>
2024-12-15 00:39:45 +0100

    create conv notif for conv photo changing

--- src/services/conversations-photos/conversations-photos.class.ts
+++ src/services/conversations-photos/conversations-photos.class.ts
@@ -66,33 +66,9 @@ export class ConversationsPhotosService<
       },
       params
     )
-    //create a notification for members
-
-    const currentUserId = params?.user._id.toString() as string
-    const updater = await app.service('my-users').get(currentUserId, { ...params, query: {} })
-
-    const notif = await app.service('messages')._create(
-      {
-        conversation: conversationId,
-        text: `${updater.name} a changé la photo de la conversation.`,
-        createdAt: new Date().toISOString(),
-        updatedAt: new Date().toISOString(),
-        type: 'notification'
-      },
-      {
-        ...params,
-        query: {}
-      }
-    )
-    const conv = await app.service('conversations').get(conversationId, params)
-    //set notification visibility for current members
-    for (const member of conv.members) {
-      await app.service('message-visibility').create({
-        userId: member._id.toString(),
-        messageId: notif._id.toString(),
-        conversationId: notif.conversation
-      })
-    }
+    await app
+      .service('messages')
+      .createNotification(conversationId, 'a changé la photo de la conversation.', params)
     return {
       message: 'File uploaded successfully',
       path: filePath
